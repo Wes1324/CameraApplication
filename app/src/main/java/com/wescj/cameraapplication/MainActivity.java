f7ca85c8ff7214d91b3b3b5884beffe4d0e360ee
Wes1324 <xtbh_5@hotmail.com>
2017-02-08 10:50:56 +0000

    Tried to show full-sie image, but not working

--- app/src/main/java/com/wescj/cameraapplication/MainActivity.java
+++ app/src/main/java/com/wescj/cameraapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.wescj.cameraapplication;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +12,16 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int TAKE_PICTURE_REQUEST_CODE = 0;
     private ImageView capturedImage;
+    private String mImageFileLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +33,38 @@ public class MainActivity extends AppCompatActivity {
     public void takePhoto(View v) {
         Intent takePictureIntent = new Intent();
         takePictureIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile)); //Tells camera where to save picture taken
+
         startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST_CODE);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == TAKE_PICTURE_REQUEST_CODE && resultCode == RESULT_OK){
             //Toast.makeText(this, "picture taken", Toast.LENGTH_SHORT).show();
-            Bundle extras = data.getExtras();
-            Bitmap photoCapturedBitmap = (Bitmap) extras.get("data");
+            //Bundle extras = data.getExtras();
+            //Bitmap photoCapturedBitmap = (Bitmap) extras.get("data");
+            //capturedImage.setImageBitmap(photoCapturedBitmap);
+            Bitmap photoCapturedBitmap = BitmapFactory.decodeFile(mImageFileLocation); //Goes to where camera saved picture and decodes it
             capturedImage.setImageBitmap(photoCapturedBitmap);
         }
+    }
+
+    File createImageFile() throws IOException {
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMAGE_" + timestamp + "_";
+
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(imageFileName, ".jpg", storageDirectory);
+
+        mImageFileLocation = imageFile.getAbsolutePath();
+
+        return imageFile;
     }
 }
